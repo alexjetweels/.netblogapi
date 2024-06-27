ae01b500fa950120f8676b611bbcf115b44f767a
Alex <alex@jetweels.com>
2024-06-27 17:37:01 +0700

    lession 9: entity genertic repo

--- Domain/Interface/IUnitOfWork.cs
+++ Domain/Interface/IUnitOfWork.cs
@@ -2,5 +2,6 @@ namespace Domain.Interface;
 
 public interface IUnitOfWork
 {
-    
+    void Commit();
+    Task CommitAsync();
 }
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+339892e8d90936e17ec9ebdedf61915fe9b27e1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1acfd4af509203565d543ffd1ec3b632692b29b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
